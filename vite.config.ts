f59af397768b71e1b5232290e76a53f9fe33814a
Azhar Mahmood <mywritingfrenzy@gmail.com>
2025-02-24 01:57:21 +0530

    Fixing Build

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+	plugins: [
+		react(),
+		checker({
+			enableBuild: true,
+		}),
+	],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	optimizeDeps: {
+		exclude: ['lucide-react'],
+	},
 });
